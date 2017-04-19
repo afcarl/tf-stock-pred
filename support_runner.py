a6531f1cee0468d4dd163351eef74756327364af
andompesta <andompesta>
2017-04-19 11:40:55 +0700

    fix fextraction form support

--- support_runner.py
+++ support_runner.py
@@ -1,7 +1,14 @@
 import dataset_preparation.feature_extraction as fe
 import dataset_preparation.dataset_extraction as de
-import matplotlib
-matplotlib.use("Pdf")
 
-fe.run()
-de.run()
+
+
+INPUT_DIR = "./data/stock"
+OUTPUT_DIR = "./data"
+COMPANY_NAME = "IBM"
+EXAMPLE_FN_NAME = "create_example_sequencial"
+OUTPUT_NAME_SUFFIX = '_seq'
+
+
+# fe.run(COMPANY_NAME, path=INPUT_DIR)
+de.run(COMPANY_NAME, EXAMPLE_FN_NAME, OUTPUT_NAME_SUFFIX, path=INPUT_DIR)
