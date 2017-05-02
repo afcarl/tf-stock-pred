Fix batch normalization to happen before activation function
Problem on deep_rnn:
-weight distribution explode -> vanishing gradient

models/deep_rnn.py
@@ -28,15 +28,20 @@ def deep_rnn(h_params, mode, features_map, target):
         filtered = dense_layer.dense_layer_over_time(filtered, in_size, h_layer_dim,
                                                      sequence_length=h_params.sequence_length,
                                                      scope_name='dense_{}'.format(layer_idx),
-                                                     activation_fn=tf.nn.sigmoid)
+                                                     activation_fn=leaky_relu)
         in_size = h_layer_dim
 
+    filtered = tf.contrib.layers.batch_norm(filtered,
+                                            center=True,
+                                            scale=True,
+                                            is_training=is_training(mode),
+                                            scope='bn')
     with tf.variable_scope('rnn') as vs:
         # Unstack to get a list of 'n_steps' tensors of shape (batch_size, n_input)
 
         # Define a lstm cell with tensorflow
         cell = tf.contrib.rnn.GRUCell(h_params.h_layer_size[-1],
-                                       activation=tf.nn.tanh)
+                                      activation=tf.nn.tanh)
         # cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=h_params.dropout)
 
         # Get lstm cell output