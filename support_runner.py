3d3fcf458d0e56f8f4fad1b86bef913de8565b38
andompesta <andograsa@gmail.com>
2017-05-11 18:30:46 +0800

    Performance extractions

--- support_runner.py
+++ support_runner.py
@@ -1,6 +1,6 @@
 import dataset_preparation.feature_extraction as fe
 import dataset_preparation.dataset_extraction as de
-
+from utils.extraction_functions import compute_return
 
 
 INPUT_DIR = "./data/stock"
@@ -8,7 +8,7 @@ OUTPUT_DIR = "./data"
 COMPANY_NAME = "apple"
 EXAMPLE_FN_NAME = "create_example_sequencial"
 OUTPUT_NAME_SUFFIX = 'seq'
-RETURN_FN = lambda x:x 
+RETURN_FN = compute_return
 
 fe.run(COMPANY_NAME, path=INPUT_DIR, return_fn=RETURN_FN)
 de.run(COMPANY_NAME, EXAMPLE_FN_NAME, OUTPUT_NAME_SUFFIX, in_path=INPUT_DIR, out_path=OUTPUT_DIR)
